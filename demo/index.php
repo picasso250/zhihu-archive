de92a7028734f557941e036955e265bf2f2db10a
xiaochi <cumt.xiaochi@gmail.com>
2014-02-24 12:07:09 +0800

    简单的搜索功能

--- demo/index.php
+++ demo/index.php
@@ -12,14 +12,17 @@ ORM::configure('username', $user);
 ORM::configure('password', $password);
 ORM::configure('driver_options', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
-$answers = ORM::forTable('answer')
+$orm = ORM::forTable('answer')
     ->join('question', array('answer.q_id', '=', 'question.id'))
     ->join('user', array('answer.user', '=', 'user.name'))
     ->select('*')
     ->select('answer.id', 'aid')
         ->whereGt('answer.vote', 1000)
-    ->limit(10)
-    ->findMany();
+    ->limit(10);
+if ($kw = _get('kw')) {
+    $orm->whereRaw('(question.title like ? or user.nick_name like ?)', array("%$kw%", "%$kw%"));
+}
+$answers = $orm->findMany();
 
 $style = _get('style', 'normal');
 
