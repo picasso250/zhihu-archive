b52f11cee145ffe755d23f44194e275213e5cca7
picasso250 <cumt.xiaochi@gmail.com>
2014-09-25 00:56:54 +0800

    default

--- fetch/python/install.sql
+++ fetch/python/install.sql
@@ -1,30 +1,27 @@
 CREATE TABLE `user` (
   `id` INTEGER PRIMARY KEY,
-  `name` TEXT,
-  `nick_name` TEXT,
-  `avatar` TEXT,
-  `fetch_time` INTEGER,
-  `fetch` INTEGER
+  `name` TEXT NOT NULL,
+  `nick_name` TEXT NOT NULL DEFAULT '',
+  `avatar` TEXT NOT NULL DEFAULT '',
+  `fetch_time` INTEGER NOT NULL DEFAULT 0,
+  `fetch` INTEGER NOT NULL DEFAULT 0
 );
 CREATE UNIQUE INDEX idx_username ON user(name);
 
 CREATE TABLE `question` (
   `id` INTEGER PRIMARY KEY,
-  `title` TEXT,
-  `description` TEXT,
-  `vote` INTEGER,
-  `fetch_time` INTEGER,
-  `create_time` INTEGER,
-  `fetch` INTEGER
+  `title` TEXT NOT NULL,
+  `description` TEXT NOT NULL,
+  `fetch_time` INTEGER NOT NULL DEFAULT 0,
+  `fetch` INTEGER NOT NULL DEFAULT 0
 );
 
 CREATE TABLE `answer` (
   `id` INTEGER PRIMARY KEY,
-  `q_id` INTEGER,
-  `user_id` INTEGER,
-  `text` TEXT,
-  `vote` INTEGER,
-  `create_time` INTEGER,
-  `fetch_time` INTEGER
+  `q_id` INTEGER NOT NULL,
+  `user_id` INTEGER NOT NULL,
+  `text` TEXT NOT NULL,
+  `vote` INTEGER NOT NULL,
+  `fetch_time` INTEGER NOT NULL DEFAULT 0
 );
 CREATE UNIQUE INDEX idx_qu ON answer(q_id, user_id);
